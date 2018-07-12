e3a1b69efabe0d527186a97f82e0010d7d7aec31
Auron <auronoda@hotmail.com>
2018-07-11 22:29:15 -0700

    finished authentication

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys.js");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+require("./models/User.js");
 require("./services/passport.js");
 
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 3 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes.js")(app);
 
 const PORT = process.env.PORT || 5000;
