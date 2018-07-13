2c6ac7556c1d9e40189a9fe6c47a64eaaa8de9bd
Auron <auronoda@hotmail.com>
2018-07-13 00:25:18 -0700

    finished payment and client prod

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 import { FETCH_USERS } from "./types";
 
-export const fetchUser = () => {
-  return async function(dispatch) {
-    const res = await axios.get("/api/current_user");
-    dispatch({
-      type: FETCH_USERS,
-      payload: res.data
-    });
-  };
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get("/api/current_user");
+  dispatch({
+    type: FETCH_USERS,
+    payload: res.data
+  });
+};
+
+export const handleToken = token => async dispatch => {
+  const res = await axios.post("/api/stripe", token);
+  dispatch({
+    type: FETCH_USERS,
+    payload: res.data
+  });
 };
--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  StripePubishableKey: process.env.STRIPE_PUBISHABLE_KEY,
+  StripeSecretKey: process.env.STRIPT_SECRET_KEY
 };
